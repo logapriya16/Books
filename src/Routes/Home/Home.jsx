8c8cae6be39a32127144f89de2acca4143fea8c1
Logapriya <slogaapriya@gmail.com>
2023-06-17 22:39:30 +0530

    liked to search page

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Home() {
+    const navigate=useNavigate()
   return (
     <div>
-      
+      <button onClick={()=>navigate('/search') }>Search</button>
     </div>
   )
 }
