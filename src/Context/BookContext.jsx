745e89bf94d2e42ec3bfe1becef2ed00b10ca8b7
Logapriya <slogaapriya@gmail.com>
2023-06-18 00:01:32 +0530

    Search feature added

--- src/Context/BookContext.jsx
+++ src/Context/BookContext.jsx
@@ -1,131 +1,76 @@
 import React, { createContext, useState } from "react";
 import { Books } from "../backend/books";
+import { v4 as uuid } from "uuid";
+
 export const BookContext = createContext();
 export default function BookProvider({ children }) {
+  const [search, setSearch] = useState(Books);
+  const SearchHandler = (text) => {
+    console.log(text);
+    setSearch(
+      Books.filter((item) =>
+        item.name.toLowerCase().includes(text.toLowerCase())
+      )
+    );
+  };
   const [shelve, setShelve] = useState({
-    currentlyReading: [],
-    wantToRead: [],
-    Read: [],
+    currentlyReading: [
+      {
+        _id: uuid(),
+
+        name: "Collected Short Stories, The (The Penguin Ray Library)",
+        author: " Satyajit Ray",
+        category: "classic-fiction",
+        img: "https://m.media-amazon.com/images/I/51U9tK6DkKL._SX325_BO1,204,203,200_.jpg",
+      },
+      {
+        _id: uuid(),
+
+        name: "The Complete Novels of Sherlock Holmes",
+        author: "  Arthur Conan Doyle",
+        category: "classic-fiction",
+        img: "https://m.media-amazon.com/images/I/61Q7MHNU9qL._SY498_BO1,204,203,200_.jpg",
+      },
+    ],
+    wantToRead: [
+      {
+        _id: uuid(),
+
+        name: "The Time Machine",
+        author: " H. G. Wells ",
+        category: "classic-fiction",
+        img: "https://m.media-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg",
+      },
+    ],
+    Read: [
+      {
+        _id: uuid(),
+
+        name: "108 Panchatantra Stories for Children",
+        author: "Maple Press",
+        category: "children",
+        img: "https://m.media-amazon.com/images/I/51trTdAFECL._SX385_BO1,204,203,200_.jpg",
+      },
+    ],
     none: [],
   });
 
-  const ShelveHandeler = (shelveType, book) => {
-    if (shelveType === "currently reading") {
-      if (shelve.wantToRead.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          currentlyReading: shelve.wantToRead.pop(book),
-        }));
-      }
-
-      if (shelve.Read.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          Read: shelve.Read.pop(book),
-        }));
-      }
-
-      if (shelve.none.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          none: shelve.none.pop(book),
-        }));
-      }
-      if(!shelve.currentlyReading.includes(book)){
-        shelve.currentlyReading.push(book)
-      setShelve((prev) => ({
-        ...prev,
-        currentlyReading: shelve.currentlyReading
-      }));
-      }
-    }
-    if (shelveType === "want to read") {
-      if (shelve.currentlyReading.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          currentlyReading: shelve.currentlyReading.pop(book),
-        }));
-      }
-
-      if (shelve.Read.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          Read: shelve.Read.pop(book),
-        }));
-      }
-
-      if (shelve.none.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          none: shelve.none.pop(book),
-        }));
-      }
-      shelve.wantToRead.push(book)
-      setShelve((prev) => ({
-        ...prev,
-        currentlyReading: shelve.wantToRead
-      }));
-    }
-    if (shelveType === "read") {
-      if (shelve.currentlyReading.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          currentlyReading: shelve.currentlyReading.pop(book),
-        }));
-      }
-      
-      if (shelve.wantToRead.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          Read: shelve.wantToRead.pop(book),
-        }));
-      }
-
-      if (shelve.none.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          none: shelve.none.pop(book),
-        }));
-      }
-      if(!shelve.Read.includes(book)){
-        shelve.Read.push(book)
-      setShelve((prev) => ({
-        ...prev,
-        currentlyReading: shelve.Read
-      }));
-      }
-    }
-    if (shelveType === "none") {
-      if (shelve.currentlyReading.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          currentlyReading: shelve.currentlyReading.pop(book),
-        }));
-      }
-
-      if (shelve.Read.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          Read: shelve.Read.pop(book),
-        }));
-      }
-
-      if (shelve.wantToRead.includes(book)) {
-        setShelve((prev) => ({
-          ...prev,
-          none: shelve.wantToRead.pop(book),
-        }));
-      }
-      shelve.none.push(book)
-      setShelve((prev) => ({
-        ...prev,
-        currentlyReading: shelve.none
-      }));
-    }
+  const IsCurrentlyReading = (item) => {
+    return shelve.currentlyReading.includes(item);
   };
+  const IsWantToRead = (item) => {
+    shelve.wantToRead.includes(item);
+  };
+  const IsRead = (item) => {
+    shelve.Read.includes(item);
+  };
+  const IsNone = (item) => {
+    shelve.none.includes(item);
+  };
+  const ShelveHandeler = (shelveType, book) => {};
   console.log(shelve);
   return (
-    <BookContext.Provider value={{ ShelveHandeler }}>
+    <BookContext.Provider value={{ ShelveHandeler, shelve, SearchHandler ,search,}}>
       {children}
     </BookContext.Provider>
   );
--- src/Routes/Search/Search.jsx
+++ src/Routes/Search/Search.jsx
@@ -4,39 +4,45 @@ import "./Search.css";
 import { BookContext } from "../../Context/BookContext";
 import { useNavigate } from "react-router-dom";
 export default function Search() {
-  const { ShelveHandeler } = useContext(BookContext);
-  const navigate=useNavigate()
+  const { ShelveHandeler, SearchHandler ,search,} = useContext(BookContext);
+  const navigate = useNavigate();
   return (
     <div className="search-container">
-      <input type="search" placeholder="search a book" />
-<button onClick={()=>navigate('/')}>Home</button>
+      <input
+        type="search"
+        placeholder="search a book"
+        onChange={(e) => SearchHandler(e.target.value)}
+      />
+      <button onClick={() => navigate("/")}>Home</button>
       <div className="books">
-       <ul className="books"> {Books.map((book) => {
-          return (
-            <li className="books-container" type="none" key={book._id} >
-              <img
-                src={book.img}
-                className="books-img"
-                alt="book-img-comes-here"
-              />
-              <select
-                name="shelve"
-                id="shelve-selection"
-                className="shelve-container"
-                onChange={(e) => ShelveHandeler(e.target.value, book )}
-              >
-                <option value="">Move to ...</option>
-                <option value="currently reading">currently reading</option>
-                <option value="want to read">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">none</option>
-              </select>
-              <p>{book.name}</p>
-              <p>{book.author}</p>
-              <p>{book.category}</p>
-            </li>
-          );
-        })}
+        <ul className="books">
+          {" "}
+          {search.map((book) => {
+            return (
+              <li className="books-container" type="none" key={book._id}>
+                <img
+                  src={book.img}
+                  className="books-img"
+                  alt="book-img-comes-here"
+                />
+                <select
+                  name="shelve"
+                  id="shelve-selection"
+                  className="shelve-container"
+                  onChange={(e) => ShelveHandeler(e.target.value, book)}
+                >
+                  <option value="">Move to ...</option>
+                  <option value="currently reading">currently reading</option>
+                  <option value="want to read">Want to Read</option>
+                  <option value="read">Read</option>
+                  <option value="none">none</option>
+                </select>
+                <p>{book.name}</p>
+                <p>{book.author}</p>
+                <p>{book.category}</p>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
